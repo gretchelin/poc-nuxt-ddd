0f14940adaf3b0cd780177565e67f3402490ac1b
Gretta M.R <gretta.marchella@mydigilearn.id>
2024-09-09 16:17:43 +0700

    build: add vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   ],
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxt/icon'],
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxt/icon', '@vueuse/nuxt'],
   eslint: {
     config: {
       stylistic: {
