9acc7da6e3d421c37916c77e39db3b209e584f6b
Gretta M.R <gretta.marchella@mydigilearn.id>
2024-09-09 16:34:40 +0700

    build: add vee validate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   ],
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxt/icon', '@vueuse/nuxt', '@nuxt/fonts'],
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxt/icon', '@vueuse/nuxt', '@nuxt/fonts', '@vee-validate/nuxt'],
   eslint: {
     config: {
       stylistic: {
