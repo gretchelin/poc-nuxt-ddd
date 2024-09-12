b0df229026ef01c9a1c672a71857a4d900c6ead1
Gretta M.R <gretta.marchella@mydigilearn.id>
2024-09-12 16:59:58 +0700

    chore: add sample component in core layer

---
+++ core/components/FetchSpinner.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="text-center space-y-4"
+  >
+    <Icon
+      name="uil-spinner"
+      width="40"
+      height="40"
+      mode="svg"
+      class="animate-spin mx-auto"
+    />
+    <slot>
+      <span>Fetching data....</span>
+    </slot>
+  </div>
+</template>
