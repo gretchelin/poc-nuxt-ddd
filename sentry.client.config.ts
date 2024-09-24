a26f69a37719b39dd5d09c9c737d7f8c62ae4e62
Gretta M.R <gretta.marchella@mydigilearn.id>
2024-09-24 14:56:57 +0700

    chore: disable sentry in development and testing env

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,9 +1,11 @@
 import * as Sentry from '@sentry/nuxt';
 
-Sentry.init({
-  dsn: process.env.NUXT_SENTRY_DSN,
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: process.env.NUXT_SENTRY_DSN,
 
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
-  tracesSampleRate: 1.0,
-});
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+  });
+}
