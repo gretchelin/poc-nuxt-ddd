a1fddc713aa655e1bfdf5c4482734c0f2983c97d
Randy Vianda Putra <aweutist@Randys-MacBook-Pro.local>
2024-12-11 07:30:49 +0700

    feat(document): add create & delete documment functionality

--- layers/document/api/document.ts
+++ layers/document/api/document.ts
@@ -78,3 +78,30 @@ export const editDocument = async (data: { title: string; description: string; t
     body: data,
   });
 };
+
+export const createDocument = async (data: { title: string; description: string; thumbnail: string; file: string }) => {
+  const authCookie = useCookie('token');
+  const env = useRuntimeConfig();
+
+  const token = authCookie?.value;
+  return await useFetch(`${env.public.apiBaseUrl}cms/v2/files/upload`, {
+    headers: {
+      Authorization: token,
+    },
+    method: 'post',
+    body: data,
+  });
+};
+
+export const deleteDocument = async (id: number) => {
+  const authCookie = useCookie('token');
+  const env = useRuntimeConfig();
+
+  const token = authCookie?.value;
+  return await useFetch(`${env.public.apiBaseUrl}cms/v2/files/upload/${id}`, {
+    headers: {
+      Authorization: token,
+    },
+    method: 'delete',
+  });
+};
