8ebd16002dd95896a9bafff040f7846926bfa0e3
Randy Vianda Putra <aweutist@Randys-MacBook-Pro.local>
2024-12-18 12:42:33 +0700

    feat: global component advanced filter & fix functionality datatable pagination & search

---
+++ ui/components/molecules/filter/index.vue
@@ -0,0 +1,120 @@
+<template>
+  <div>
+    <div class="flex items-center gap-4">
+      <Icon
+        name="mdi-search"
+        class="filter--icon"
+        width="22"
+        height="22"
+        mode="svg"
+      />
+      <input
+        type="text"
+        :placeholder="placeholder"
+        class="-ml-12 pl-10 px-4 py-2 border border-gray-300 rounded-md w-full focus:border-teal-500 focus:outline-none"
+        @keyup="handleSearch"
+      >
+      <UIButton
+        color="secondary"
+        class="text-sm text-gray-500"
+        @click="toggleFilter"
+      >
+        <img
+          src="public/img/icons/filter.svg"
+          alt="filter"
+        >
+        Filter
+      </UIButton>
+    </div>
+
+    <div
+      v-show="isFilterVisible"
+      class="filter"
+    >
+      <div class="flex items-center space-x-2">
+        <slot name="advanced-filter" />
+      </div>
+
+      <!-- Action buttons -->
+      <div class="filter--actions">
+        <span
+          class="mr-2 cursor-pointer"
+          @click="handleReset"
+        >
+          Reset
+        </span>
+        <UIButton
+          color="secondary"
+          :class="['border']"
+          @click="toggleFilter"
+        >
+          Cancel
+        </UIButton>
+        <UIButton
+          class="px-4 py-2 bg-teal-500 text-white rounded-md text-sm hover:bg-teal-600"
+          @click="handleApply"
+        >
+          Apply
+        </UIButton>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import UIButton from '#ui/components/atoms/button';
+
+interface IProps {
+  id: string;
+  placeholder: string;
+  isAdvanced: boolean;
+  handleSearch: () => void;
+  handleApply: () => void;
+  handleReset: () => void;
+}
+
+const props = withDefaults(defineProps<IProps>(), {
+  isAdvanced: false,
+  placeholder: '',
+});
+
+const isFilterVisible = ref(false);
+
+const toggleFilter = () => {
+  isFilterVisible.value = !isFilterVisible.value;
+};
+
+const handleSearch = (e: any) => {
+  if (props.handleSearch) {
+    props.handleSearch(e.target.value);
+  }
+};
+
+const handleReset = () => {
+  if (props.handleReset) {
+    props.handleReset();
+  }
+};
+
+const handleApply = () => {
+  if (props.handleApply) {
+    props.handleApply();
+  }
+};
+</script>
+
+<style scoped>
+.filter {
+  @apply flex flex-col py-3 px-4 border shadow rounded my-2 gap-2 right-1 bg-white absolute;
+}
+
+.filter--icon {
+  @apply text-gray-400;
+  margin-left: 10px;
+  z-index: 5
+}
+
+.filter--actions {
+  @apply flex items-center space-x-2 border-t pt-2 mt-1 justify-end
+}
+</style>
